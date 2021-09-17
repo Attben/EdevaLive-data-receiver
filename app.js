c9b43b52e3dc7ad763021c2f5000ff614af3983e
Attben <72492246+Attben@users.noreply.github.com>
2021-09-17 12:04:32 +0200

    Basic handling of POST requests containing JSON data.

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const app = express();
 
 const http = require('http').createServer(app);
 
+app.use(express.json());
 const PORT = 8080;
 
 app.get('/', (req, res) => {
     console.log("Incoming connection");
     res.sendFile(__dirname + '/Pages/index.html');
+});
+
+app.post('/', (req, res) => {
+    let requestBody = req.body;
+    console.log(requestBody);
+    requestBody.Text = "Hello, ".concat(requestBody.Text);
+    res.send(req.body);
 });
 
 http.listen(PORT, () => {
