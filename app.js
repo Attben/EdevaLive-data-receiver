0ef3dc31f25e4a41f60c0de6f83946a0a5e24379
Attben <72492246+Attben@users.noreply.github.com>
2021-09-17 11:11:29 +0200

    Added node code skeleton (with Express and Socket.io)

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('Hello world');
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    console.log("Incoming connection");
+    res.sendFile(__dirname + '/Pages/index.html');
+});
+
+http.listen(PORT, () => {
+    console.log(__dirname);
+    console.log("Hello, world!");
+});
